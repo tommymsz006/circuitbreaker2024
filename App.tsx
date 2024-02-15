d8efbe863388e1e6b85059ccbf28b0ef4ff3c215
tommymsz006 <37748412+tommymsz006@users.noreply.github.com>
2024-02-15 20:06:14 +0800

    major changes around contracts and UI framework - add basic ERC-4337 constructs and user operation - integrate with gluestack UI framework - use dotenv (react native version) for environment variable management

--- App.tsx
+++ App.tsx
@@ -1,166 +1,134 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState, useRef } from 'react';
+import { NativeModules } from 'react-native';
 
-import React from 'react';
-import {NativeModules, Button} from 'react-native';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {  GluestackUIProvider,
+          Text,
+          Button,
+          ButtonText,
+          ButtonIcon,
+          ButtonSpinner,
+          AddIcon,
+          VStack,
+          Divider,
+          Heading,
+          Card
+} from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
 
 // Import the crypto getRandomValues shim (**BEFORE** the shims)
 import "react-native-get-random-values";
 // Import the the ethers shims (**BEFORE** ethers)
 import "@ethersproject/shims";
+
 // Import Semaphore components
 import { Identity } from "@semaphore-protocol/identity";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+// Import others
+import { ethers } from "ethers";
+//import { Presets } from "userop";
+import { SimpleAccount } from './src/builder/simpleAccount';
 
 async function onIdentityButtonPress() {
-  const {SemaphoreEnclaveModule} = NativeModules;
-  console.log(SemaphoreEnclaveModule);
-
-  const idAlias = 'account.001';
-  
-  console.log('Try to authenticate');
-  await SemaphoreEnclaveModule.authenticate(idAlias); 
-  console.log('Authentication invoked');
-
-  // create key pair first
-  let keyPair = await SemaphoreEnclaveModule.createKeyPair(idAlias);
-  console.log(`A key pair for alias ${idAlias} has been generated with public key: ${keyPair.publicKey}`);
-
-  // deterministically create identity from a secret message
-  let identityString = await SemaphoreEnclaveModule.createSecuredIdentityString(idAlias);
-  console.log(`An identity string for alias: ${idAlias} has been generated: ${identityString.base}`);
-  const identity = new Identity(identityString.base);
-  console.log(`Trapdoor: ${identity.trapdoor}`);
-  console.log(`Nullifier: ${identity.nullifier}`);
-  console.log(`Commitment: ${identity.commitment}`);
 }
 
 function IdentityButton(): JSX.Element {
-  const onPress = () => {
+  const [isInProgress, setIsInProgress] = useState(false);
+
+  const onPress = async() => {
+    setIsInProgress(true);
     console.log('We will invoke identity creation here..!');
-    onIdentityButtonPress();
-    console.log('Invoked!');
+    
+    const {SemaphoreEnclaveModule} = NativeModules;
+    console.log(SemaphoreEnclaveModule);
+
+    const idAlias = 'account.001';
+    
+    console.log('Try to authenticate');
+    await SemaphoreEnclaveModule.authenticate(idAlias); 
+    console.log('Authentication invoked');
+
+    // create key pair first
+    let keyPair = await SemaphoreEnclaveModule.createKeyPair(idAlias);
+    console.log(`A key pair for alias ${idAlias} has been generated with public key: ${keyPair.publicKey}`);
+
+    // deterministically create identity from a secret message
+    let identityString = await SemaphoreEnclaveModule.createSecuredIdentityString(idAlias);
+    console.log(`An identity string for alias: ${idAlias} has been generated: ${identityString.base}`);
+    const identity = new Identity(identityString.base);
+    console.log(`Trapdoor: ${identity.trapdoor}`);
+    console.log(`Nullifier: ${identity.nullifier}`);
+    console.log(`Commitment: ${identity.commitment}`);
+
+    setIsInProgress(false);
   };
 
   return (
     <Button
-      title="Click to create a new secured identity!"
-      color="#841584"
+      size="md"
+      variant="solid"
+      action="primary"
+      isDisabled={false}
+      isFocusVisible={false}
       onPress={onPress}
-    />
+    >
+      <ButtonText>Identity </ButtonText>
+      <ButtonIcon as={AddIcon} />
+      <ButtonSpinner animating={isInProgress} />
+    </Button>
+  );
+};
+
+function SmartContractAccountButton(): JSX.Element {
+  const [isInProgress, setIsInProgress] = useState(false);
+
+  const onPress = async() => {
+    setIsInProgress(true);
+    if (process.env.ERC4337_ACCOUNT_SIGNING_KEY && process.env.NODE_RPC_URL) {
+      console.log(`We're about to create a new smart contract wallet via node RPC ${process.env.NODE_RPC_URL}`);
+      const account = await SimpleAccount.init(
+        new ethers.Wallet(process.env.ERC4337_ACCOUNT_SIGNING_KEY),
+        process.env.NODE_RPC_URL
+      );
+      console.log(`Wallet address is ${account.getSender()}`);
+    } else {
+      console.error("No default signing key and/or node RPC address defined")
+    }
+    setIsInProgress(false);
+  };
+
+  return (
+    <Button
+      size="md"
+      variant="solid"
+      action="primary"
+      isDisabled={isInProgress}
+      isFocusVisible={false}
+      onPress={onPress}
+    >
+      <ButtonText>Smart Contract Account </ButtonText>
+      <ButtonIcon as={AddIcon} />
+      <ButtonSpinner animating={isInProgress} />
+    </Button>
   );
 };
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+    <GluestackUIProvider config={config}>
+      <VStack space="md">
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+        <Card size="md" variant="elevated"  m="$3">
+          <Heading mb="$1" size="lg">Welcome to circuitbreaker2024!</Heading>
+          <Text size="sm">Create your own privacy-first wallet</Text>
+        </Card>
+        <Divider my="$0.5"/>
         <IdentityButton/>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        <SmartContractAccountButton/>
+      </VStack>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
